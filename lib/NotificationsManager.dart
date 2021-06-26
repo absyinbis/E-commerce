51bcc9ce8e5adcf1817f90ce9f58e420b698ef68
absyinbis <60608662+absyinbis@users.noreply.github.com>
2021-06-26 16:49:36 +0300

    Update NotificationsManager.dart

--- lib/NotificationsManager.dart
+++ lib/NotificationsManager.dart
@@ -36,6 +36,30 @@ class PushNotificationsManager {
           ?.createNotificationChannel(channel);
 
       await FirebaseMessaging.instance.requestPermission();
+
+      FirebaseMessaging.onMessage.listen((message) {
+        RemoteNotification notification = message.notification;
+        AndroidNotification android = message.notification?.android;
+          flutterLocalNotificationsPlugin.show(
+              notification.hashCode,
+              notification.title,
+              notification.body,
+              NotificationDetails(
+                android: AndroidNotificationDetails(
+                  channel.id,
+                  channel.name,
+                  channel.description,
+                  // TODO add a proper drawable resource to android, for now using
+                  //      one that already exists in example app.
+                  icon: 'launch_background',
+                ),
+                iOS: IOSNotificationDetails(
+                  subtitle: "subtitle",
+                ),
+              ));
+
+
+        });
       String token = await FirebaseMessaging.instance.getToken();
       print("FirebaseMessaging token: $token");
 
